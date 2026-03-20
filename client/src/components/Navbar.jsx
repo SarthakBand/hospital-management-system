b86deb790e6c7f476aebfa140921e92f14dbbe0a
Sarthak <sarthakband23@gmail.com>
2026-03-20 19:04:56 +0530

    Creating Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState,useRef } from 'react'
+import { navbarStyles } from '../assets/dummyStyles'
+import { useNavigate, useLocation, Link } from 'react-router-dom'
+import { useClerk } from '@clerk/react'
+import logo from '../assets/logo.png'
+
+
+
+const STORAGE_KEY = 'doctorToken_v1'
 
 const Navbar = () => {
+    const [isOpen, setisOpen] = useState(false)
+    const [showNavbar, setshowNavbar] = useState(true)
+    const [lastScrollY, setlastScrollY] = useState(0)
+    const [isDoctorLoggedIn, setIsDoctorLoggedIn] = useState(() => {
+        try {
+            return Boolean(localStorage.getItem(STORAGE_KEY))
+        } catch {
+            return  false
+        }
+    });
+    const location = useLocation()
+    const navRef = useRef(null)
+    const clerk = useClerk()
+    const navigate = useNavigate()
   return (
-    <div>Navbar</div>
+    <>
+    <div className={navbarStyles.navbarBorder}></div>
+
+    <nav className= { `${navbarStyles.navbarContainer} ${
+        showNavbar ? navbarStyles.navbarVisible : navbarStyles.navbarHidden
+    }`}
+    >
+        <div className={navbarStyles.contentWrapper}>
+            <div className={navbarStyles.flexContainer}>
+                {/* Logo */ }
+                <Link to='/' className={navbarStyles.logoLink}>
+                <div className={navbarStyles.logoContainer}>
+                    <div className={navbarStyles.logoImageWrapper}>
+                        <img src={logo} alt="Logo" className={navbarStyles.logoImage} />
+                    </div>
+                </div>
+                </Link>
+            </div>
+        </div>
+        
+        
+    </nav>   
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+<ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} >
+<BrowserRouter>
 
     <App />
 
   </BrowserRouter>
+</ClerkProvider>
+
 
 )
